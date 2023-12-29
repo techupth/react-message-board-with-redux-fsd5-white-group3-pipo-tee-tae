45de6049443a040843c489875289b3379f7acab1
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-29 16:19:23 +0700

    feat(exercise): add completed exercise

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,54 @@
+import { useSelector, useDispatch } from "react-redux";
+import { getText, addList, deleteList } from "../slices/messageBoardSlice";
+
 function MessageBoard() {
+  const stateText = useSelector((state) => {
+    return state.stateText;
+  });
+
+  const dispatch = useDispatch();
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(event) => {
+              dispatch(getText(event.target.value));
+            }}
+            value={stateText.text}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button
+          className="submit-message-button"
+          onClick={() => {
+            dispatch(addList());
+          }}
+        >
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {stateText.textList.map((item, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  dispatch(deleteList(index));
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
